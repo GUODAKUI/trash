f75bc5107d9748a520d543abb5b99ed864d4d443
khuanghonka <khuanghonka@gmail.com>
2013-10-21 22:08:03 +0800

    delete foot
    ugly

--- globals/foot.php
+++ globals/foot.php
@@ -3,8 +3,7 @@
            		<table width="100%">
                 <tr align="center"><td>
                 <span>Copyright © 东南大学计算机科学与工程学院&nbsp;&nbsp;丨</span>
-                <span><a href="<?php echo  $GLOBALS['PATH']?>manage/login.php">后台管理</a></span>
-                <span style="margin-left: 300px;">技术支持：科索(Cosersoft)软件公司</span>
+                <span><a href="./manage/login.php">后台管理</a></span>
                 </td></tr>
                 </table>
            </div>
